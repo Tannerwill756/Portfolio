d90df1c6d18d4e508b44bd24f59a2832d4cb1858
Tanner Williams <tannerwill756@gmail.com>
2020-08-10 19:15:07 -0400

    ran build script

---
+++ build/precache-manifest.98410e6affd41d4ba49a21601bef47ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91290ce6439f7ebdc2744479d6b16ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082cbece2ad073a3a8af",
+    "url": "/static/css/main.0180e0e0.chunk.css"
+  },
+  {
+    "revision": "f831c103e615b324c1f9",
+    "url": "/static/js/2.e0256316.chunk.js"
+  },
+  {
+    "revision": "082cbece2ad073a3a8af",
+    "url": "/static/js/main.e7bae072.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71eefc2be22989a466069a18a2bb9340",
+    "url": "/static/media/Profile-Pic.71eefc2b.jpg"
+  },
+  {
+    "revision": "de6f87e3730231db107f9cb4d1288189",
+    "url": "/static/media/coMake.de6f87e3.PNG"
+  },
+  {
+    "revision": "b70048d2ff7ae5fd2c5dfc828a0e77bb",
+    "url": "/static/media/debut_light.b70048d2.png"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/static/media/ep_naturalblack.23e2b6cf.png"
+  },
+  {
+    "revision": "c4117654e75679439380965481d1334e",
+    "url": "/static/media/expat.c4117654.PNG"
+  },
+  {
+    "revision": "41310c25b26712d95b5ee78c1f880a31",
+    "url": "/static/media/pipes.41310c25.png"
+  },
+  {
+    "revision": "79d5d9e18ecf157eb1353dd3ff360d31",
+    "url": "/static/media/randChamp.79d5d9e1.PNG"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.faf9fb7023fa1b8b81c934f52dafc555.js"
+  "/precache-manifest.98410e6affd41d4ba49a21601bef47ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
